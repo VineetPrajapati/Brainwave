b46614302191c4ca58b2b7d75917f936a7a843cd
Vineet <infinite2202@gmail.com>
2024-03-11 23:25:31 +0530

    react-tailwind

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Benifits from "./components/Benifits";
 import Collaboration from "./components/Collaboration";
 import Header from "./components/Header";
 import Hero from "./components/Hero";
+import Services from "./components/Services";
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
         <Hero />
         <Benifits />
         <Collaboration />
+        <Services />
       </div>
       <ButtonGradient />
     </>
