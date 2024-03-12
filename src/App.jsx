3f6f8f988be013fe4e717ee0217325768865d9ec
Vineet <infinite2202@gmail.com>
2024-03-12 11:35:18 +0530

    react-tailwind

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Benifits from "./components/Benifits";
 import Collaboration from "./components/Collaboration";
 import Header from "./components/Header";
 import Hero from "./components/Hero";
+import Pricing from "./components/Pricing";
 import Services from "./components/Services";
 
 function App() {
@@ -15,6 +16,7 @@ function App() {
         <Benifits />
         <Collaboration />
         <Services />
+        <Pricing />
       </div>
       <ButtonGradient />
     </>
--- src/components/Benifits.jsx
+++ src/components/Benifits.jsx
@@ -9,14 +9,14 @@ const Benifits = () => {
     <Section id="features">
       <div className="container relativez-2">
         <Heading
-          className="md:max-w-md lg:max-w-2xl"
+          className="m-auto md:max-w-md lg:max-w-2xl"
           title="Chat Smarter, Not Harder with Brainwave"
         />
 
         <div className="flex flex-wrap gap-10 mb-10">
           {benefits.map((item) => (
             <div
-              className="block relative p-0.5 bg-no-repeat bg-[length:100%_100%] md:max-w-[24rem]"
+              className="m-auto block relative p-0.5 bg-no-repeat bg-[length:100%_100%] md:max-w-[24rem]"
               style={{
                 backgroundImage: `url(${item.backgroundUrl})`,
               }}
